9af057a4c0406072c9e9ebd5859ae6b5cb5eb4f0
Jason Pincin <jason@pincin.com>
2015-11-03 17:29:42 -0500

    no need for such verbosity

--- post.js
+++ post.js
@@ -1,7 +1,7 @@
 module.exports = function post (callee, postCall) {
     return function callHook () {
         return postCall.apply({
-            previousReturnValue: callee.apply(undefined, arguments)
+            returnValue: callee.apply(undefined, arguments)
         }, arguments)
     }
 }
